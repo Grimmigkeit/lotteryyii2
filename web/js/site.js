32c48692a56014c088a4e0fe289357e73df19373
Serj <serj@serj.domain>
2018-12-10 00:46:03 +0200

    ADD: ajax for lottery

--- web/js/site.js
+++ web/js/site.js
@@ -1,1 +1,24 @@
- 
+$( document ).ready( function() {
+
+    $('.-random').on('click', function(e) {
+        e.preventDefault();
+
+        var $self = $(this);
+
+        $self.attr('disabled', true);
+
+        $.post("/index.php?r=post/lottery", function() {})
+        .done(function(data) {
+            console.log(data);
+            alert( "success" );
+        })
+        .fail(function() {
+            alert( "error" );
+        })
+        .always(function() {
+            $self.attr('disabled', false);
+        });
+
+    });
+
+});
